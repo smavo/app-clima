f8588af2a5fddc41ac4eb21d6edfdbaf83d056f9
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-05-31 22:02:02 -0500

    Obteniendo Data de Api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
-    direccion:{
+    direccion: {
         alias: 'd',
         diesc: 'Direccion de la cuidad para obtener el clima',
         demmand: true
@@ -7,3 +9,19 @@ const argv = require('yargs').options({
 }).argv;
 
 console.log(argv.direccion);
+
+const encodedUlr = encodeURI( argv.direccion );
+//console.log(encodedUlr);
+    
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodedUlr }`,
+    headers: {'X-RapidAPI-Key': 'YrIv9XHJxmmshCBitpg1YTAnahQSp1KbdHhjsnSBU1hvMDMlzK'}
+  });
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.Results[0]);
+    })
+    .catch(err => {
+        console.log('Error', err);
+    })
